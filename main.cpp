52484e556cfc47e8f2548d43f7a4330c62bc3bbd
Ibrahim Bello <ibrbello10@gmail.com>
2026-05-20 18:56:38 -0700

    10 minute timer

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <list>
-#include <vector>
+#include <queue>
 #include <deque>
 #include <array>
 #include <string>
@@ -13,7 +13,7 @@ const int ARR_SIZE = 50;
 
 void print_deque(deque<string> const &);
 void print_vector(vector<string> const &);
-void print_arr(array<string, ARR_SIZE> const &);
+void print_queue(queue<string> const &);
 
 int main() {
     cout << "\n=========BREAKFAST BOOTH QUEUE SIMULATION==========\n";
@@ -21,13 +21,13 @@ int main() {
     DoublyLinkedList coffeeLine;
     deque<string> muffinsLine;
     vector<string> braceletsLine;
-    array<string, ARR_SIZE> croissantsLine;
+    queue<string> croissantsLine;
 
     for (int i = 0; i < 3; i++) {
         coffeeLine.push_back();
         muffinsLine.push_back(names[rand() % NUM_NAMES]);
         braceletsLine.push_back(names[rand() % NUM_NAMES]);
-        croissantsLine[croissantsLine.size()] = names[rand() % NUM_NAMES];
+        croissantsLine.push(names[rand() % NUM_NAMES]);
         cout << endl;
     }
 
@@ -40,7 +40,7 @@ int main() {
     print_vector(braceletsLine);
     cout << endl;
     cout << "Initial croissant shop queue:\n";
-    print_arr(croissantsLine);
+    print_queue(croissantsLine);
     cout << endl;
     
     for (int i = 1; i <= NUM_ROUNDS; i++ ) {
@@ -62,17 +62,15 @@ int main() {
 
         if (croissantsLine.empty()) cout << "There's no one to serve in the croissant shop line.\n";
         else { 
-            // Not sure how to easily delete the front person and shift every other person over
-            // croissantsLine[0] = ;
+            croissantsLine.pop();
             cout << "Bracelet stand customer served.\n";
         }
 
-        // 50% chance of a customer joining
+        // 50% chance of a customer joining any of the lines
         int prob = rand() % 100;
         if (prob > 50) {
             coffeeLine.push_back();
         }
-        // Make probabilities independent
         int prob2 = rand() % 100;
         if (prob2 > 50) {
             muffinsLine.push_back(names[rand() % NUM_NAMES]);
@@ -85,21 +83,28 @@ int main() {
             cout << "Customer joined the bracelet stand line.\n";
         }
 
+        int prob4 = rand() % 100;
+        if (prob4 > 50) {
+            croissantsLine.push(names[rand() % NUM_NAMES]);
+            cout << "Customer joined the croissant shop line.\n";
+        }
+
         cout << "\nCurrent coffee line: \n";
         coffeeLine.print();
         cout << "\nCurrent muffin stand line:\n";
         print_deque(muffinsLine);
         cout << "\nCurrent bracelet stand line:\n";
         print_vector(braceletsLine);
+        cout << "\nCurrent croissant shop line:\n";
+        print_queue(croissantsLine);
     }
 
-
-    // Milestone 5: add array queue of croissants
+    // Arrays are finicky. Maps and sets automatically sort, so that doesn't fit the 
+    // store queue use case. Graphs and hash tables don't really work either
+    // std:queue has been studied in the Zybook, so I think it counts as a structure 
+    // studied in the course
+    // Milestone 5: add std::queue queue for croissant stand
     // Once again, only one type of croissant, so everyone's order is the same
-    // Not sure how to make this work. Let's try a binary tree
-    // Create binary tree (straightforward)
-    // Add random names to BST
-
 
     return 0;
 }
@@ -107,11 +112,9 @@ int main() {
 void print_deque(deque<string> const & dq) {
     for (const auto & item: dq) cout << item << endl;
 }
-
 void print_vector(vector<string> const & vec) {
     for (const auto & item : vec) cout << item << endl;
 }
-
-void print_arr(array<string, ARR_SIZE> const & arr) {
-    for (const auto & item : arr) cout << item << endl;
+void print_queue(queue<string> const & q) {
+    for (const auto & item : q) cout << item << endl;
 }
