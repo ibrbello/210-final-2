8ab3dd701094ceb4afab2f5be72e8733c751bac9
Ibrahim Bello <ibrbello10@gmail.com>
2026-05-20 17:25:16 -0700

    10 minute update

--- DLL.h
+++ DLL.h
@@ -21,13 +21,17 @@ class DoublyLinkedList {
     
         Node* head;
         Node* tail;
+
+// Operations I need:
+// Push back, pop front
+// No searching needed, no deleting values
     
     public:
         // constructor
         DoublyLinkedList() { head = nullptr; tail = nullptr; }
     
         void push_back(string nm, string drink) {
-            Node* newNode = new Node(value);
+            Node* newNode = new Node(nm, drink);
             if (!tail)  // if there's no tail, the list is empty
                 head = tail = newNode;
             else {
@@ -35,110 +39,6 @@ class DoublyLinkedList {
                 newNode->prev = tail;
                 tail = newNode;
             }
-        }
-    
-        void push_front(string nm, string drink) {
-            Node* newNode = new Node(value);
-            if (!head)  // if there's no head, the list is empty
-                head = tail = newNode;
-            else {
-                newNode->next = head;
-                head->prev = newNode;
-                head = newNode;
-            }
-        }
-    
-        void insert_after(string nm, string drink, int position) {
-            if (position < 0) { // input check
-                cout << "Position must be >= 0." << endl;
-                return;
-            }
-    
-            Node* newNode = new Node(value);
-            if (!head) { 
-                head = tail = newNode;
-                return;
-            }
-    
-            Node* temp = head;
-            for (int i = 0; i < position && temp; ++i)
-                temp = temp->next;
-    
-            if (!temp) {
-                cout << "Position exceeds list size. Node not inserted.\n";
-                delete newNode;
-                return;
-            }
-    
-            newNode->next = temp->next;
-            newNode->prev = temp;
-            if (temp->next)
-                temp->next->prev = newNode;
-            else
-                tail = newNode; // Inserting at the end
-            temp->next = newNode;
-        }
-    
-        void delete_val(string nm, string drink) {
-            if (!head) {
-                cout << "List is empty." << endl;
-                return;
-            }
-    
-            Node* temp = head;
-            while (temp && temp->data != value)
-                temp = temp->next;
-    
-            if (!temp) {
-                cout << "Value not found." << endl << endl;
-                return;
-            } 
-    
-            if (temp->prev) {
-                temp->prev->next = temp->next;
-            } else {
-                head = temp->next; // Deleting the head
-            }
-    
-            if (temp->next) {
-                temp->next->prev = temp->prev;
-            } else {
-                tail = temp->prev; // Deleting the tail
-            }
-    
-            delete temp;
-            cout << "Node deleted." << endl << endl;
-        }
-
-        void delete_pos(int pos) {
-            if (!head) {
-                cout << "List is empty." << endl;
-                return;
-            }
-    
-            Node* temp = head;
-            for (int i = 0; i < pos; i++) {
-                temp = temp->next;
-                if (!temp) { // Check if temp is nullptr before it goes out of range
-                    cout << "Position is outside of list range. Node not deleted.";
-                    cout << endl << endl;
-                    return;
-                }
-            }
-            if (temp->prev) {
-                temp->prev->next = temp->next;
-            } else {
-                head = temp->next; // Deleting the head
-            }
-    
-            if (temp->next) {
-                temp->next->prev = temp->prev;
-            } else {
-                tail = temp->prev; // Deleting the tail
-            }
-    
-            delete temp;
-            cout << "Node deleted." << endl << endl;
         }
 
         void pop_front() {
@@ -154,24 +54,11 @@ class DoublyLinkedList {
     
         }
 
-        void pop_back() {
-            if (!tail) {
-                cout << "List is empty." << endl;
-                return;
-            }
-            Node* temp = tail;
-            temp->prev->next = nullptr; 
-            tail = temp->prev;
-            delete temp;
-            cout << "Node deleted." << endl << endl;
-    
-        }
-    
         void print() {
             Node* current = head;
             if (!current) return;
             while (current) {
-                cout << current->data << " ";
+                cout << current->name << " " << current->drink << endl;
                 current = current->next;
             }
             cout << endl << endl;
@@ -181,7 +68,7 @@ class DoublyLinkedList {
             Node* current = tail;
             if (!current) return;
             while (current) {
-                cout << current->data << " ";
+                cout << current->name << " " << current->drink << endl;
                 current = current->prev;
             }
             cout << endl << endl;
@@ -194,6 +81,126 @@ class DoublyLinkedList {
                 delete temp;
             }
         }
+    
+        // void push_front(string nm, string drink) {
+        //     Node* newNode = new Node(value);
+        //     if (!head)  // if there's no head, the list is empty
+        //         head = tail = newNode;
+        //     else {
+        //         newNode->next = head;
+        //         head->prev = newNode;
+        //         head = newNode;
+        //     }
+        // }
+    
+        // void insert_after(string nm, string drink, int position) {
+        //     if (position < 0) { // input check
+        //         cout << "Position must be >= 0." << endl;
+        //         return;
+        //     }
+    
+        //     Node* newNode = new Node(value);
+        //     if (!head) { 
+        //         head = tail = newNode;
+        //         return;
+        //     }
+    
+        //     Node* temp = head;
+        //     for (int i = 0; i < position && temp; ++i)
+        //         temp = temp->next;
+    
+        //     if (!temp) {
+        //         cout << "Position exceeds list size. Node not inserted.\n";
+        //         delete newNode;
+        //         return;
+        //     }
+    
+        //     newNode->next = temp->next;
+        //     newNode->prev = temp;
+        //     if (temp->next)
+        //         temp->next->prev = newNode;
+        //     else
+        //         tail = newNode; // Inserting at the end
+        //     temp->next = newNode;
+        // }
+    
+        // void delete_val(string nm, string drink) {
+        //     if (!head) {
+        //         cout << "List is empty." << endl;
+        //         return;
+        //     }
+    
+        //     Node* temp = head;
+        //     while (temp && temp->data != value)
+        //         temp = temp->next;
+    
+        //     if (!temp) {
+        //         cout << "Value not found." << endl << endl;
+        //         return;
+        //     } 
+    
+        //     if (temp->prev) {
+        //         temp->prev->next = temp->next;
+        //     } else {
+        //         head = temp->next; // Deleting the head
+        //     }
+    
+        //     if (temp->next) {
+        //         temp->next->prev = temp->prev;
+        //     } else {
+        //         tail = temp->prev; // Deleting the tail
+        //     }
+    
+        //     delete temp;
+        //     cout << "Node deleted." << endl << endl;
+        // }
+
+        // void delete_pos(int pos) {
+        //     if (!head) {
+        //         cout << "List is empty." << endl;
+        //         return;
+        //     }
+    
+        //     Node* temp = head;
+        //     for (int i = 0; i < pos; i++) {
+        //         temp = temp->next;
+        //         if (!temp) { // Check if temp is nullptr before it goes out of range
+        //             cout << "Position is outside of list range. Node not deleted.";
+        //             cout << endl << endl;
+        //             return;
+        //         }
+        //     }
+        //     if (temp->prev) {
+        //         temp->prev->next = temp->next;
+        //     } else {
+        //         head = temp->next; // Deleting the head
+        //     }
+    
+        //     if (temp->next) {
+        //         temp->next->prev = temp->prev;
+        //     } else {
+        //         tail = temp->prev; // Deleting the tail
+        //     }
+    
+        //     delete temp;
+        //     cout << "Node deleted." << endl << endl;
+        // }
+
+
+
+        // void pop_back() {
+        //     if (!tail) {
+        //         cout << "List is empty." << endl;
+        //         return;
+        //     }
+        //     Node* temp = tail;
+        //     temp->prev->next = nullptr; 
+        //     tail = temp->prev;
+        //     delete temp;
+        //     cout << "Node deleted." << endl << endl;
+    
+        // }
+    
     };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -8,6 +8,31 @@
 #include "DLL.h"
 using namespace std;
 
+const int NUM_NAMES = 50;
+const std::string names[NUM_NAMES] = {
+    "James",   "Oliver",  "Noah",    "Liam",    "Ethan",
+    "Lucas",   "Mason",   "Logan",   "Aiden",   "Jackson",
+    "Emma",    "Sophia",  "Olivia",  "Ava",     "Isabella",
+    "Mia",     "Harper",  "Amelia",  "Evelyn",  "Abigail",
+    "Carlos",  "Jimmy",  "Devon",   "Jordan",  "Tyler",
+    "Brandon", "Trevor",  "Derek",   "Dustin",  "Kyle",
+    "Amber",   "Brianna", "Chelsea", "Tiffany", "Kristen",
+    "Megan",   "Ashley",  "Lauren",  "Shannon", "Brooke",
+    "Ahmed",   "Diego",   "Marcus",  "Victor",  "Andre",
+    "Priya",   "Maya",    "Leila",   "Elena",   "Nadia"
+};
+
+const int NUM_DRINKS = 20;
+const std::string drinks[NUM_DRINKS] = {
+    "Espresso",           "Americano",          "Flat white",
+    "Cold brew",          "Oat latte",          "Cortado",
+    "Cappuccino",         "Matcha latte",        "Iced mocha",
+    "Macchiato",          "Vanilla latte",       "Drip coffee",
+    "Caramel cortado",    "Dirty chai",          "Pour over",
+    "Ristretto",          "Honey latte",         "Nitro cold brew",
+    "Iced americano",     "Hazelnut cappuccino"
+};
+
 // Steps:
 // 1. Create Node struct
 //  
