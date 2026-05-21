900e4d8f1f7e1424d5fce45ee7e2eaddef4ec216
Ibrahim Bello <ibrbello10@gmail.com>
2026-05-20 17:04:58 -0700

    10-minute update

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// COMSC-210 | Final Part 2 | Ibrahim Bello
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <deque>
+#include <string>
+using namespace std;
+
+struct Node {
+    string name;
+    string drink;
+    Node* next;
+};
+
+
+// Steps:
+// 1. Create Node struct
+//  
+// 2. Create data arrays with names and drink orders (use LLM, 50 names & 20 orders)
+
+
+int main() {
+
+
+    return 0;
+}
